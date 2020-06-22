fcfef5ea6372af4b1d0804778a482094a5079f72
denis-tingajkin <denis.tingajkin@xored.com>
2020-06-22 12:56:29 +0700

    add test to cover example from golangci config file

--- analyzer_test.go
+++ analyzer_test.go
@@ -155,3 +155,47 @@ See the License for the specific language governing permissions and
 limitations under the License.`))
 	require.Nil(t, issue)
 }
+
+func TestGolangCIExample(t *testing.T) {
+	a := goheader.New(
+		goheader.WithTemplate(`{{ AUTHOR }} {{ MY COMPANY }} {{ YEAR }}
+SPDX-License-Identifier: Apache-2.0
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at:
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.`),
+		goheader.WithValues(map[string]goheader.Value{
+			"MY COMPANY": &goheader.ConstValue{
+				RawValue: "mycompany.com",
+			},
+			"YEAR": &goheader.ConstValue{
+				RawValue: "2020",
+			},
+			"AUTHOR": &goheader.RegexpValue{
+				RawValue: `.*@mycompany\.com`,
+			},
+		}))
+	issue := a.Analyze(header(`author@mycompany.com mycompany.com 2020
+SPDX-License-Identifier: Apache-2.0
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at:
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.`))
+	require.Nil(t, issue)
+}
